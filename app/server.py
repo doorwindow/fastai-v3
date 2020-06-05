cf27a307897c381b6c0408ab5b0406ac02f43525
doorwindow <66462376+doorwindow@users.noreply.github.com>
2020-06-05 13:09:13 +0530

    Update server.py

--- app/server.py
+++ app/server.py
@@ -9,10 +9,10 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
-export_file_url = 'https://www.dropbox.com/s/6bgq8t6yextloqp/export.pkl?raw=1'
+export_file_url = 'https://drive.google.com/uc?export=download&id=1qFJqlNZSmH2HiRkfxaBUe8J1SjjQ6os5'
 export_file_name = 'export.pkl'
 
-classes = ['black', 'grizzly', 'teddys']
+classes = ['acoustic', 'electric', 'archtop', 'resonator', 'double_neck']
 path = Path(__file__).parent
 
 app = Starlette()
